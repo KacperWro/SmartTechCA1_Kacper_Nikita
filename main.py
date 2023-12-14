9ef2b7a446c9823f40f67cdea301c63bfbb39943
KacperWro <adamczyk911119@gmail.com>
2023-12-14 18:39:48 +0000

    Import cifar-10 data

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# Main code
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+data = unpickle("cifar-10-batches-py")
+
+print(data)
