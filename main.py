8f466e78b72f5a62ee7e1e556730e937e760a80e
KacperWro <adamczyk911119@gmail.com>
2023-12-15 12:15:57 +0000

    Get count per label before and after data cleaning

--- main.py
+++ main.py
@@ -21,19 +21,9 @@ cifar10_unwanted_labels = [cifar10_label_names[b'label_names'][0],
                            cifar10_label_names[b'label_names'][8]]
 
 
-#print(cifar10_unwanted_labels)
-#print(cifar10_label_names)
-#print(cifar10_data_batch_1.keys())
-
 cifar100_test = unpickle("cifar-100-python/test")
 cifar100_train = unpickle("cifar-100-python/train")
 cifar100_meta = unpickle("cifar-100-python/meta")
-
-print(cifar100_test.keys())
-#print(cifar100_meta.keys())
-#print(cifar100_meta[b'fine_label_names'])
-#print(cifar100_test[b'fine_labels'])
-#print(cifar100_test[b'coarse_labels'])
 
 
 def merge_dicts(dict_list):
@@ -47,6 +37,7 @@ def replace_numerical_labels_with_names(dict_list):
         numerical_label = dict_list[b'labels'][i]
         dict_list[b'labels'][i] = cifar10_label_names[b'label_names'][numerical_label]
     return dict_list
+
 
 def replace_numerical_labels_with_fine_names(dict_list):
     for i in range(len(dict_list[b'fine_labels'])):
@@ -71,36 +62,45 @@ def remove_unwanted_labels(dict_list):
     dict_list[b'data'] = np.array(dict_list[b'data'], dtype=np.uint8)
     return dict_list
 
+
+def count_labels(dict_list):
+    for i in range(len(cifar10_label_names[b'label_names'])):
+        counter = 0
+
+        for j in range(len(dict_list[b'labels'])):
+            if dict_list[b'labels'][j] == cifar10_label_names[b'label_names'][i]:
+                counter = counter + 1
+
+        print(cifar10_label_names[b'label_names'][i], " count: ", counter)
+
+
 cifar10_dict_list = [cifar10_data_batch_1, cifar10_data_batch_2, cifar10_data_batch_3, cifar10_data_batch_4, cifar10_data_batch_5]
 cifar10_merged_dict = merge_dicts(cifar10_dict_list)
 
-#print(len(cifar10_merged_dict[b'labels']))
-#print(len(cifar10_merged_dict[b'data']))
-#print(len(cifar10_merged_dict[b'data'][1]))
+print("Labels length", len(cifar10_merged_dict[b'labels']))
+print("Data rows length", len(cifar10_merged_dict[b'data']))
+print("Data columns length", len(cifar10_merged_dict[b'data'][1]))
 
 img = cifar10_merged_dict[b'data'][100]
 reshaped_image = np.reshape(img, (32, 32, 3), order='F')
 plt.imshow(reshaped_image)
 plt.show()
 
-#print(cifar10_label_names[b'label_names'][1])
-
 cifar10_merged_dict = replace_numerical_labels_with_names(cifar10_merged_dict)
-#print(cifar10_merged_dict[b'labels'][1])
-#print(type(cifar10_merged_dict[b'labels'][1]))
 
 cifar100_train = replace_numerical_labels_with_fine_names(cifar100_train)
-print(cifar100_train[b'fine_labels'])
 
-#print(cifar10_merged_dict.keys())
+print("\nLength of labels before removing unwanted labels:", len(cifar10_merged_dict[b'labels']))
+print("Length of data before removing unwanted labels:", len(cifar10_merged_dict[b'data']))
 
-#print(len(cifar10_merged_dict[b'labels']))
-#print(len(cifar10_merged_dict[b'data']))
-# print(cifar10_label_names[b'label_names'][0] in cifar10_merged_dict[b'data'])     This line causes a futurewarning
+print("\nCOUNTS PER LABEL BEFORE")
+count_labels(cifar10_merged_dict)
+
 cifar10_merged_dict = remove_unwanted_labels(cifar10_merged_dict)
-#print(len(cifar10_merged_dict[b'labels']))
-#print(len(cifar10_merged_dict[b'data']))
+print("\nLength of labels after removing unwanted labels:", len(cifar10_merged_dict[b'labels']))
+print("Length of data after removing unwanted labels:", len(cifar10_merged_dict[b'data']))
 
-
+print("\nCOUNTS PER LABEL AFTER")
+count_labels(cifar10_merged_dict)
 
 
