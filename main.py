db03a17a1117c53d556be595c68cbe8677689255
Nfedans <nfedans@gmail.com>
2023-12-15 13:25:39 +0000

    purge unwanted data from Cifar-100

--- main.py
+++ main.py
@@ -25,6 +25,20 @@ cifar100_test = unpickle("cifar-100-python/test")
 cifar100_train = unpickle("cifar-100-python/train")
 cifar100_meta = unpickle("cifar-100-python/meta")
 
+cifar100_wanted_fine_labels = [
+    cifar100_meta[b'fine_label_names'][19],# cattle
+    cifar100_meta[b'fine_label_names'][34],# fox
+    cifar100_meta[b'fine_label_names'][41],# lawnmower
+    cifar100_meta[b'fine_label_names'][65],# rabbit
+    cifar100_meta[b'fine_label_names'][80],# squirrel
+    cifar100_meta[b'fine_label_names'][89]# tractor
+]
+
+cifar100_wanted_coarse_labels = [
+    cifar100_meta[b'coarse_label_names'][14], # baby, boy, girl, man, woman
+    cifar100_meta[b'coarse_label_names'][17], # maple, oak, palm, pine, willow
+    cifar100_meta[b'coarse_label_names'][18]  # bicycle, bus, motorcycle, pickup truck, train
+]
 
 def merge_dicts(dict_list):
     merged_labels = [label for d in dict_list for label in d[b'labels']]
@@ -41,10 +55,11 @@ def replace_numerical_labels_with_names(dict_list):
 
 def replace_numerical_labels_with_fine_names(dict_list):
     for i in range(len(dict_list[b'fine_labels'])):
-        numerical_label = dict_list[b'fine_labels'][i]
-        dict_list[b'fine_labels'][i] = cifar100_meta[b'fine_label_names'][numerical_label]
+        numerical_fine_label = dict_list[b'fine_labels'][i]
+        dict_list[b'fine_labels'][i] = cifar100_meta[b'fine_label_names'][numerical_fine_label]
+        numerical_coarse_label = dict_list[b'coarse_labels'][i]
+        dict_list[b'coarse_labels'][i] = cifar100_meta[b'coarse_label_names'][numerical_coarse_label]
     return dict_list
-
 
 def remove_unwanted_labels(dict_list):
     dict_list[b'data'] = dict_list[b'data'].tolist()
@@ -62,6 +77,23 @@ def remove_unwanted_labels(dict_list):
     dict_list[b'data'] = np.array(dict_list[b'data'], dtype=np.uint8)
     return dict_list
 
+
+def remove_unwanted_labels_cifar_100(dict_list):
+    dict_list[b'data'] = dict_list[b'data'].tolist()
+
+    if len(dict_list[b'fine_labels']) == len(dict_list[b'data']) and len(dict_list[b'fine_labels']) == len(dict_list[b'coarse_labels']):
+        counter = 0
+        while counter < len(dict_list[b'data']):
+            if dict_list[b'coarse_labels'][counter] not in cifar100_wanted_coarse_labels and dict_list[b'fine_labels'][counter] not in cifar100_wanted_fine_labels:
+                del dict_list[b'fine_labels'][counter]
+                del dict_list[b'coarse_labels'][counter]
+                del dict_list[b'data'][counter]
+            else:
+                counter = counter + 1
+    else:
+        print("ERROR: Length of labels is not the same as length of data")
+    dict_list[b'data'] = np.array(dict_list[b'data'], dtype=np.uint8)
+    return dict_list
 
 def count_labels(dict_list):
     for i in range(len(cifar10_label_names[b'label_names'])):
@@ -89,6 +121,14 @@ plt.show()
 cifar10_merged_dict = replace_numerical_labels_with_names(cifar10_merged_dict)
 
 cifar100_train = replace_numerical_labels_with_fine_names(cifar100_train)
+cifar100_train = remove_unwanted_labels_cifar_100(cifar100_train)
+
+print(len(cifar100_train[b'coarse_labels']))
+
+print(len(cifar100_train[b'fine_labels']))
+
+print(len(cifar100_train[b'data']))
+
 
 print("\nLength of labels before removing unwanted labels:", len(cifar10_merged_dict[b'labels']))
 print("Length of data before removing unwanted labels:", len(cifar10_merged_dict[b'data']))
